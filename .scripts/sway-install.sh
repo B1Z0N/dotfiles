8fb8dfd0d2765911ac8d6110946dab508063be7f
Mykola Fedurko <kolausf@gmail.com>
2022-05-30 21:48:33 +0300

    Started sway setup script and added todos.

---
+++ .scripts/sway-install.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+sudo apt install build-essential
+sudo apt install git
+sudo apt install python3-pip
+
+pip3 install --user meson==0.55.3
+# export PATH=$HOME/.local/bin:$PATH
+# source ~/.bashrc
+
+sudo apt install wayland-protocols \
+libwayland-dev \
+libegl1-mesa-dev \
+libgles2-mesa-dev \
+libdrm-dev \
+libgbm-dev \
+libinput-dev \
+libxkbcommon-dev \
+libgudev-1.0-dev \
+libpixman-1-dev \
+libsystemd-dev \
+cmake \
+libpng-dev \
+libavutil-dev \
+libavcodec-dev \
+libavformat-dev \
+ninja-build \
+meson
+
+sudo apt install libxcb-composite0-dev \
+        libxcb-icccm4-dev \
+        libxcb-image0-dev \
+        libxcb-render0-dev \
+        libxcb-xfixes0-dev \
+        libxkbcommon-dev \
+        libxcb-xinput-dev \
+        libx11-xcb-dev
+
+mkdir ~/sway-build
+cd ~/sway-build
+git clone https://github.com/swaywm/wlroots.git
+cd wlroots
+git checkout 0.12.0
+
+meson build -Dxwayland=enable
+ninja -C build
+sudo ninja -C build install
+
+sudo apt install libjson-c-dev \
+libpango1.0-dev \
+libcairo2-dev \
+libgdk-pixbuf2.0-dev \
+scdoc
+
+cd ~/sway-build
+git clone https://github.com/swaywm/sway.git
+cd sway
+git checkout 1.5.1
+
+meson build -Dxwayland=enable
+ninja -C build
+sudo ninja -C build install
+
+
+
+### installing extras
+
+# locker
+sudo apt install swaylock 
+
+# dmenu for wayland
+cd ~/sway-build
+git clone https://github.com/nyyManni/dmenu-wayland
+cd dmenu-wayland
+mkdir build
+meson build
+ninja -C build
+sudo ninja -C build install
+
+# alacritty
+sudo add-apt-repository ppa:mmstick76/alacritty
+sudo apt update
+sudo apt install alacritty
+
+# screenshots(see grimshot already in .local/bin)
+sudo apt install wl-clipboard
+sudo apt install grim
+sudo apt install slurp
+sudo apt install jq
+
+# notifications
+# usage: notify-send hello
+sudo apt install mako-notifier
+
+# blurred lock screen
+sudo apt install ffmpeg
+
+# TODO
+sleep on lid close
+volume
+brightness
+multiscreen
+bluetooth
+wifi
+redshift
