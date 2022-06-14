514d7190ac76e4951cae9eb315525ad2305be694
Fedurko <mfedurko@luxoft.com>
2022-06-14 13:17:23 +0300

    Added scripting to powershell.

---
+++ .scripts/system-setup.ps1
@@ -0,0 +1,20 @@
+# install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# install vim 
+choco install vim
+
+# install fzf
+choco install fzf
+
+# install ripgrep
+choco install ripgrep
+
+# install git
+choco install git
+
+# allow running scripts like ps1
+Set-ExecutionPolicy RemoteSigned
+
+# install oh my posh for powershell
+winget install JanDeDobbeleer.OhMyPosh
